d84d1034dbae37abe080d001447bf8a15bbfd224
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2024-08-20 22:03:48 +0530

    Created a method to save a new item into the database using model mapper.

--- src/main/java/com/chethanawijesinghe/pointOfSales/service/serviceImpl/ItemIMPL.java
+++ src/main/java/com/chethanawijesinghe/pointOfSales/service/serviceImpl/ItemIMPL.java
@@ -1,4 +1,33 @@
 package com.chethanawijesinghe.pointOfSales.service.serviceImpl;
 
-public class ItemIMPL {
+import com.chethanawijesinghe.pointOfSales.dto.RequestItemSaveDTO;
+import com.chethanawijesinghe.pointOfSales.entity.Item;
+import com.chethanawijesinghe.pointOfSales.repository.ItemRepo;
+import com.chethanawijesinghe.pointOfSales.service.ItemService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemIMPL implements ItemService {
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Autowired
+    private ItemRepo itemRepo;
+
+
+    //to save items
+    @Override
+    public void addItem(RequestItemSaveDTO requestItemSaveDTO) {
+        Item item = modelMapper.map(requestItemSaveDTO, Item.class);
+        if (!itemRepo.existsById(item.getItemId())) {
+            itemRepo.save(item);
+
+        } else {
+            throw new DuplicateKeyException("Already in the list");
+        }
+    }
 }
