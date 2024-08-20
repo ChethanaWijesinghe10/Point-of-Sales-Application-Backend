0ad2dc794801935cf50a3e61cfd2a3c42d4c577a
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2024-08-20 21:35:31 +0530

    Updated

--- src/main/java/com/chethanawijesinghe/pointOfSales/entity/MeasuringUnitType.java
+++ src/main/java/com/chethanawijesinghe/pointOfSales/entity/MeasuringUnitType.java
@@ -1,5 +1,8 @@
 package com.chethanawijesinghe.pointOfSales.entity;
 
+import org.springframework.context.annotation.Configuration;
+
+
 public enum MeasuringUnitType {
     KILO_GRAM,LITER,MILLILITER,NUMBER,GRAM;
 }
